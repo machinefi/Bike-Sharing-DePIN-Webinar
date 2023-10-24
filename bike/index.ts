7f46c9636131bef901c609e721954b56331c3a84
simonerom <mail@sromano.com>
2023-10-25 00:04:41 +0200

    Introduced bike id in the bike message

--- bike/index.ts
+++ bike/index.ts
@@ -1,7 +1,7 @@
 import { W3bstreamClient } from "w3bstream-client-js";
 
-const URL = "COPY/PASTE FROM WS STUDIO PROJECT EVENTS TAB";
-const API_KEY = "CREATE ONE IN WS STUDIO ACCOUNT SETTINGS";
+const URL = "W3BSTREAM PROJECT HTTP ROUTE";
+const API_KEY = "W3BSTREAM ACCOUNT API KEY";
 
 const client = new W3bstreamClient(URL, API_KEY);
 
@@ -13,7 +13,7 @@ const header = {
 // Set your wallet address as the bike owner
 const payload = {
   data: `{
-        "bike_owner": "0x2C37a2cBcFacCdD0625b4E3151d6260149eE866B",
+        "bike_id": "0x2300ee8d4d4661138be2c7a3a0497086fcdd2aba94f5b9dcfb9169f497024e35",
         "ride_start": "${Date.now()}",
         "ride_duration": 3620,
         "ride_distance": 1500
@@ -24,6 +24,9 @@ main();
 
 async function main() {
   try {
+    /*
+     * We should sign the data field of the payload with the private key of the bike. 
+    */
     const res = await client.publishSingle(header, Buffer.from(payload.data));
 
     console.log("Response:");
